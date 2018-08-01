a25d3b49ab85663698486e81c6ccee92af6a6d34
avanishkumar00 <avanishkumar163@gmail.com>
2018-08-01 18:53:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    
+    return render_template('index.html')
+
+@app.route('/bootstrap')
+def home():
+
+    return render_template('bootstrap.html')
+
+app.run()
